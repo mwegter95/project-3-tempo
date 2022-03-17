73e305d643410d6d943be20b822476d966438418
adairconlin <adair.conlin@gmail.com>
2022-03-17 14:22:15 -0400

    Render reviews on individual user page

--- tempo/src/pages/MyReviews.js
+++ tempo/src/pages/MyReviews.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_MY_REVIEWS } from "../utils/queries";
 import ReviewList from "../components/ReviewList";
 
 const MyReviews = () => {
-    const { loading, data } = useQuery(QUERY_MY_REVIEWS);
+    // create state for reviews
+    const [ myReviews, setMyReviews ] = useState(null)
+    const getMyReviews = (data) => {
+        return data?.myReviews;
+    };
 
-    console.log("my review data", data);
-    let reviews = data?.reviews || [];
+    // query for reviews created by user
+    const { loading, data, error } = useQuery(QUERY_MY_REVIEWS, {
+        onCompleted: (data) => setMyReviews(getMyReviews(data))
+    });
+
+    let reviews = myReviews || [];
 
     if(loading) {
-        return <div>loading...</div>
+        return <div className="serif para">loading...</div>
     }
 
     return (
         <div>
+            <h1 className="sans-serif para">Your reviews</h1>
             <ReviewList reviews={reviews} />
         </div>
     )
