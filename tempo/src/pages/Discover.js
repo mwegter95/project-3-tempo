c8880465819a3436ef8f04feeed2f1d06d20d03a
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-03-19 15:14:16 -0500

    stopped for demo

--- tempo/src/pages/Discover.js
+++ tempo/src/pages/Discover.js
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_USERS } from "../utils/queries";
-import UserList from "../components/UserList";
+import { MUSIC_FEED } from "../utils/queries";
+import DiscoFeed from "../components/discoveryFeed";
+import MetaList from "../components/metaList";
 
+//TODO: Add Meta value entry
+//TODO: Add metaList component
+//TODO: Integrate metaListComponent
+//TODO: Add meta value bread crumbs
+//TODO: Add feedMusic to query
+//TODO: Call feedMusic
+//TODO: build discoMedia component
+//TODO: integrate discoMedia
+
+
+//Define State to include a list of metadata values that will drive the discovery media feed
+const searchMeta = () => {
+    const [metaState, setMetaState] = useState(
+        {
+            values: []
+        }
+    )};
+
+const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setMetaState({
+        ...metaState,
+        [name]: value
+    });
+};
+
+
+//render page
 const Discover = () => {
-    const { loading, data } = useQuery(QUERY_USERS);
-    const users = data?.users || [];
-
-    if(loading) {
-        return <div>loading...</div>
-    };
-
+        
     return (
         <div className="main">
             <h1 className="sans-serif para">Discover Page</h1>
             <div>
-                <UserList users={users} />
+                <form onSubmit={handleAddMeta} autoComplete="off">
+                    <div>
+                        <h1 className="sans-serif white para">What would you like to find?</h1>
+                                                
+                        <label htmlFor="addMeta" className="sans-serif white subpara">Search For:</label>
+                        <input id="addMeta" name="addMeta" className="sans-serif sm" onBlur={handleChange} />
+                        
+                        
+                        <button onClick={checkRadios} className="sans-serif sm">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <DiscoMedia feedMedia={feedMedia} />
             </div>
         </div>
     )
